a71800bdda5caaedf3289b31324924f45558da02
Daniel <daniel.sofran@stud.ubbcluj.ro>
2022-07-13 14:32:55 +0300

    baza de date, react try, nonreact end

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
+import datetime
+
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    nume = models.CharField(blank=False, max_length=50, default="Angajat")
+    email = models.EmailField(blank=True, null=False)
+    telefon = models.CharField(blank=True, null=False, default='0'*10, max_length=15)
+    role = models.CharField(blank=False, default="Angajat", max_length=20)
+
+class Info(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    lognitude = models.DecimalField(max_digits=9, decimal_places=6)
+    time = models.DateTimeField(blank=True, default=datetime.datetime.strptime("01.01.2022 00:00:00", "%d.%m.%Y %H:%M:%S"))
+    observatie = models.TextField(blank=True, max_length=300)
